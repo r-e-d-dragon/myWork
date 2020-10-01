87ed3e040f42b123112362bdc870d00a5ff288d7
r-e-d-dragon <H1705635@H1705635>
2020-10-02 08:02:18 +0900

    メール

---
+++ web-s/src/main/java/com/enjoygolf24/online/web/controller/SendMailController.java
@@ -0,0 +1,113 @@
+package com.enjoygolf24.online.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.enjoygolf24.api.common.database.bean.TblAsp;
+import com.enjoygolf24.api.common.utility.LoginUtility;
+import com.enjoygolf24.api.common.validator.groups.All;
+import com.enjoygolf24.api.service.AspService;
+import com.enjoygolf24.api.service.MemberInfoManageService;
+import com.enjoygolf24.online.web.form.SendMailForm;
+
+
+@Controller
+@RequestMapping("/admin/booking/sendMail")
+public class SendMailController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SendMailController.class);
+
+	@Autowired
+	HttpServletRequest request;
+
+	@Autowired
+	MemberInfoManageService memberInfoManageService;
+
+	@Autowired
+	AspService aspService;
+
+	@RequestMapping(value = "")
+	public String sendMailInfo(
+			@ModelAttribute("sendMailForm") SendMailForm form, Model model) {
+		logger.info("Start sendMailInfo Controller");
+
+		initForm(form, model);
+
+		logger.info("End sendMailInfo Controller");
+		return "/admin/booking/sendMail/index";
+	}
+
+	@RequestMapping(value = "/finish", method = RequestMethod.POST)
+	public String sendMailFinish(@ModelAttribute @Validated(All.class) SendMailForm form, BindingResult result,
+			Model model) {
+		logger.info("Start sendMailFinish Controller");
+
+		if (result.hasErrors()) {
+			model.addAttribute("sendMailForm", form);
+			return sendMailInfo(form, model);
+		}
+
+		// TODO 受信メールアドレス
+		String toMail = "enjoygolf24@cock.li";
+
+		try {
+			Email email = new SimpleEmail();
+			// TODO メールサーバ
+			// email.setHostName("pop3.lolipop.jp");
+			// TODO Cockmail
+			email.setHostName("mail.cock.li");
+
+			// Smtp-Port
+			email.setSmtpPort(465);
+			// TODO user-id, password
+			email.setAuthenticator(new DefaultAuthenticator("enjoygolf24@cock.li", "Passw0rd."));
+			email.setDebug(true);
+			// (SSL)
+			email.setSSLOnConnect(true);
+
+			email.setFrom(toMail);
+			email.setSubject(form.getTitle());
+			email.setMsg(form.getComment());
+			email.addTo(form.getToMail());
+
+			email.send();
+		} catch (Exception e) {
+			// 異常 - メール送信失敗
+			model.addAttribute("sendMailForm", form);
+			result.rejectValue("memberName", "error.memberName", "{0} : メール送信に失敗しました。");
+			return sendMailInfo(form, model);
+		}
+
+		logger.info("End sendMailFinish Controller");
+		return "/admin/booking/sendMail/finish";
+	}
+
+	/**
+	 * メール送信フォーム初期処理
+	 * 
+	 * @param form
+	 * @return
+	 */
+	private void initForm(SendMailForm form, Model model) {
+		String aspCode = LoginUtility.getLoginUser().getAspCode();
+		TblAsp asp = aspService.getAsp(aspCode);
+
+		form.setAspCode(aspCode);
+		form.setLoginUserCd(LoginUtility.getLoginUser().getMemberCode());
+
+		model.addAttribute("aspName", asp.getAspName());
+	}
+}
---
+++ web-s/src/main/java/com/enjoygolf24/online/web/form/RecvMailForm.java
@@ -0,0 +1,38 @@
+package com.enjoygolf24.online.web.form;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
+import com.enjoygolf24.api.common.validator.annotation.Email;
+import com.enjoygolf24.api.common.validator.groups.Insert0;
+
+import lombok.Data;
+
+@Data
+//@GroupSequence({BasicInfo.class, AdvanceInfo.class})
+public class RecvMailForm implements Serializable {
+
+	private static final long serialVersionUID = -937204629996741357L;
+
+	@NotBlank(groups = Insert0.class)
+	String aspCode;
+	String memberCode;
+
+	@NotBlank(groups = Insert0.class)
+	@Email(groups = Insert0.class)
+	String fromMail;
+	@Email(groups = Insert0.class)
+	String toMail;
+	@NotBlank(groups = Insert0.class)
+	String memberName;
+	@NotBlank(groups = Insert0.class)
+	String title;
+	@NotBlank(groups = Insert0.class)
+	String comment;
+
+	String loginUserCd;
+	String updateDate;
+	String updateUserCd;
+
+}
